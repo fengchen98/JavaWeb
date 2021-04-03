d84190b201da4165191b7f40c52c3c01bdeacdcd
fengchen98 <249841036@qq.com>
2021-04-03 11:51:10 +0800

    servlet3

--- 20210401/servlet/src/main/java/HelloServlet.java
+++ 20210401/servlet/src/main/java/HelloServlet.java
@@ -2,7 +2,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.print.Printable;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -10,6 +12,9 @@ public class HelloServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter writer=response.getWriter();
+        writer.println("hello  servlet!!!!");
     }
 }
